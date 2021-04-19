aa81da05aec830550f45a2e6a44325e00e65f7ed
Markkus Motta <markkus.motta@gmail.com>
2021-04-19 17:01:24 +0100

    add Checkbox shared component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
+import Checkbox from '../../shared/Checkbox'
 import AppContainer from '../AppContainer'
 import AppHeader from '../AppHeader'
 import { Container, Wrapper } from './App.styles'
 
 function App() {
+    const [lettuce, setLettuce] = React.useState(true)
+    const [rice, setRice] = React.useState(false)
+
+
     return <Wrapper>
         <Container>
             <AppHeader />
             <AppContainer 
-            left={<div style={{ backgroundColor: 'red'}}>Produtos Disponíveis</div>}
-            middle={<div style={{ backgroundColor: 'aqua'}}>Sua lista de compras</div>}
-            right={<div style={{ backgroundColor: 'coral'}}>Estatísticas</div>}           
+            left={<div >Produtos Disponíveis
+                <Checkbox 
+                    value={lettuce}
+                    title="Acerola"
+                    onClick={() => setLettuce(!lettuce)}
+                />
+                <Checkbox 
+                    value={rice}
+                    title="Arroz"
+                    onClick={() => setRice(!rice)}
+                />
+                <Checkbox 
+                    value={true}
+                    title="Carne"
+                />
+            </div>}
+            middle={<div >Sua lista de compras
+
+            </div>}
+            right={<div >Estatísticas
+
+            </div>}           
             />
          </Container>
     </Wrapper>
---
+++ src/shared/Checkbox/Checkbox.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    height: 32px;
+
+    display: flex;
+    align-items: center;
+    cursor: default;
+`
+
+export const Indicator = styled.div`
+    height: 14px;
+    width: 14px;
+
+    ${props => props.value ? 'background-color: #004D61' : ''};
+    border-radius: 50%;
+
+    border: 2px solid #004D61;
+    margin-right: 8px;
+    cursor: pointer; 
+    
+`
